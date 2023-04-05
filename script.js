e3cb0f6f4f835591f4e0b7eabf7d5177c7b72705
Andriy-Kolokolov <koloandre22@gmail.com>
2023-04-05 17:12:15 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+// // PROMPT SOLUTION
+// const inputKilometres = prompt("What is your travel distance? : ");
+// const inputAge = prompt("Enter your age? : ");
+//
+// const adultAge = 65;
+// const underageAge = 17;
+// const kilometrePrice = 0.21;
+//
+// function getTicketPriceAdult(kilometres) {
+// }
+//
+// function getTicketPriceUnderage(kilometres) {
+//
+// }
+// // END PROMPT SOLUTION
