bbef3f8589407ae07833798ca2d6c246a25c01bf
Andriy-Kolokolov <koloandre22@gmail.com>
2023-04-06 12:17:28 +0200

    add form input solution

--- script.js
+++ script.js
@@ -1,6 +1,43 @@
-// PROMPT SOLUTION
-const inputKilometres = prompt("What is your travel distance? : ");
-const inputAge = prompt("Enter your age? : ");
+// PROMPT SOLUTION --------------------------------------------------------------------------------
+
+// const inputKilometres = prompt("What is your travel distance? : ");
+// const inputAge = prompt("Enter your age? : ");
+//
+// const kilometrePrice = 0.21;
+// const juniorAge = 17;
+// const seniorAge = 65;
+// const discountJunior = 0.20; // 20%
+// const discountSenior = 0.40; // 40%
+//
+// if (inputAge <= juniorAge) {
+//     window.alert("Your ticket price is: " + getTicketPriceJunior(inputKilometres) + " $");
+// }
+//
+// else if (inputAge > juniorAge && inputAge < seniorAge) {
+//     window.alert("Your ticket price is: " + getTicketPriceAdult(inputKilometres) + " $")
+// }
+//
+// else if (inputAge >= seniorAge) {
+//     window.alert("Your ticket price is: " + getTicketPriceSenior(inputKilometres) + " $");
+// }
+//
+// function getTicketPriceDefault() {
+//     return inputKilometres * kilometrePrice;
+// }
+//
+// function getTicketPriceJunior() {
+//     return getTicketPriceDefault() - getTicketPriceDefault() * discountJunior;
+// }
+//
+// function getTicketPriceAdult() {
+//     return getTicketPriceDefault();
+// }
+//
+// function getTicketPriceSenior() {
+//     return getTicketPriceDefault() - getTicketPriceDefault() * discountSenior;
+// }
+
+// END PROMPT SOLUTION ---------------------------------------------------------------------
 
 const kilometrePrice = 0.21;
 const juniorAge = 17;
@@ -8,31 +45,25 @@ const seniorAge = 65;
 const discountJunior = 0.20; // 20%
 const discountSenior = 0.40; // 40%
 
-if (inputAge <= juniorAge) {
-    window.alert("Your ticket price is: " + getTicketPriceJunior(inputKilometres) + " $");
-}
+const btnGetPrice = document.getElementById("btn-calculate-price");
+const fieldPrice = document.getElementById("price-field");
 
-else if (inputAge > juniorAge && inputAge < seniorAge) {
-    window.alert("Your ticket price is: " + getTicketPriceAdult(inputKilometres) + " $")
-}
+btnGetPrice.addEventListener("click", function (event){
+    event.preventDefault();
+    const inputDistance = document.getElementById("input-travel-distance").value;
+    const inputAge = document.getElementById("input-age").value;
+    fieldPrice.innerText = getTravelPrice(inputAge, inputDistance).toFixed(2) + " €";
+})
 
-else if (inputAge >= seniorAge) {
-    window.alert("Your ticket price is: " + getTicketPriceSenior(inputKilometres) + " $");
-}
+function getTravelPrice(age, distance) {
 
-function getTicketPriceDefault() {
-    return inputKilometres * kilometrePrice;
-}
+    const defaultPrice = distance * kilometrePrice;
 
-function getTicketPriceJunior() {
-    return getTicketPriceDefault() - getTicketPriceDefault() * discountJunior;
+    if (age <= juniorAge) {
+        return defaultPrice - defaultPrice * discountJunior;
+    } else if (age > juniorAge && age < seniorAge) {
+        return defaultPrice;
+    } else if (age >= seniorAge) {
+        return defaultPrice - defaultPrice * discountSenior;
+    }
 }
-
-function getTicketPriceAdult() {
-    return getTicketPriceDefault();
-}
-
-function getTicketPriceSenior() {
-    return getTicketPriceDefault() - getTicketPriceDefault() * discountSenior;
-}
-// END PROMPT SOLUTION
